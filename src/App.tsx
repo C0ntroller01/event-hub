051a566c47d604859da9e26c1aac495ecc5b8378
Daniel Promise <promizzdaniel@gmail.com>
2026-09-10 11:06:40 +0100

    feat: initialize React application with basic structure and styling
    - Added main application component (App.tsx) with Sidebar component.
    - Created Sidebar component for future navigation.
    - Implemented CSS reset and default styles in index.css.
    - Added utility functions for class name management in utils.tsx.
    - Set up main entry point (main.tsx) for rendering the application.
    - Created Home page component placeholder (Home.tsx).
    - Configured TypeScript settings with tsconfig files for app and node.
    - Set up Vite configuration with React and Babel plugins.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Sidebar from "./components/Sidebar";
+import "./index.css"
+
+function App() {
+
+  return (
+    <div >
+      <aside className="">
+        <Sidebar/>
+      </aside>
+      <div>
+        <header className="">
+
+        </header>
+      </div>
+    </div>
+    )
+}
+
+export default App
